b389cbfadb7a6a2bf91a9c8ed304864c3aefb087
GenkiHirano <genki86web@gmail.com>
2022-09-21 22:34:29 +0900

    feat: 検索機能追加

--- main.go
+++ main.go
@@ -32,4 +32,11 @@ func main() {
 	// 認証
 	// envファイルに置き換える
 	anaconda.NewTwitterApiWithCredentials("1254473461310713857-2mg3A9L46BXF5cJma3v7Rvq1sIJFEC", "3iq3muHujMiHCMc1TLxiCrFBlfwS7uS61qdRgeDoKach4", "oIOmfUk3qk4CFNVBMMNQmMeN7", "daaKq64kqwDtiKxS8orUSPqjPhmmZFAc3ogC6GjdVnpkLBajr0")
+
+	// 検索 ["ライトコード"]
+	// TODO: apiディレクトリにGetSearch実装予定
+	searchResult, _ := api.GetSearch(`"検索キーワード"`, nil)
+	for _, tweet := range searchResult.Statuses {
+		fmt.Println(tweet.Text)
+	}
 }
